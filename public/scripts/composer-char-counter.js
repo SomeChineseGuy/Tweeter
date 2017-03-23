fc54861cded4a0be372ec2ffd6b30e3ee9e507f2
vagrant <alvin.cl.ng@gmail.com>
2017-03-23 01:53:29 +0000

    Added other users

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(function(){
+
+
+var maxLength = 140;
+$('textarea').keyup(function(){
+   var length = $(this).val().length;
+   length = maxLength-length;
+   $('.counter').text(length);
+    if (length < 0){
+       $('.counter').css('color', 'red');
+   }
+   else{
+       next();
+   }
+});
+
+});  
